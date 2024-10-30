e5b0b73cc44343299bea96b4af8ad632d7f3a7fe
Abraxas-7 <arber.beshaj.02@gmail.com>
2024-10-30 14:45:16 +0100

    consegna bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,10 +10,27 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = teachers.slice().reverse();    //faccio una copia con slice per non modificare quello originale e uso reverse per invertirlo
+console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = teachers.filter((teacher) => teacher.length >= 5);  
+console.log(longNames);
+
+/*
+  filter crea un nuovo array che contiene gli elementi che soddifano una condizione, non cambia l'array originale
+  se ritorna true l'elemento viene incluso nel nuovo array, altrimenti con false viene escluso
+  - teacher: è l'elemento corrente dell'array (nome insegnante)
+  - teacher.length: calcola la lunghezza dell'elemento corrente
+  - teacher.length >= 5: verifica se la lunghezza del elemetno (nome insegnanet) è maggiore o uguale a 5, se vero l'elemento viene incluso nel array longNames
+*/
 
 // 3. Rimuovi 'Ed' dall'array teachers
+const index = teachers.indexOf('Ed');
+
+if(index !== -1) {    //indefOf ritorna -1 se con torova l'elemento
+    teachers.splice(index, 1);    //se con indexOf trova lelemento cercato, toglie quell'elemento (1 -> elemento singolo) all'arrray all'indice index
+}
+
+console.log(teachers);
